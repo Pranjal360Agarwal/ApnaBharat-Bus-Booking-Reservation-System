c2e095a74e74fc161a4b66187fb273de4b4ef3ef
Pranjal Agarwal <98935622+Pranjal360Agarwal@users.noreply.github.com>
2022-12-08 14:11:44 +0530

    Create ContributorsList.js

---
+++ ContributorsList.js
@@ -0,0 +1,7 @@
+let contributors = [
+  {
+    github: "https://github.com/Pranjal360Agarwal",
+    name: "Pranjal Agarwal",
+    linkedin: "https://www.linkedin.com/in/pranjal-agarwal-11b543226/",
+  },
+  ];
