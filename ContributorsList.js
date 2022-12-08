0e896220d1db1a859bab8b848d004b82436fa6b0
shrey141102 <shrey14112002@gmail.com>
2022-12-08 16:09:23 +0530

    Update ContributorsList.js
    I have added my details in the ContributorsList.js file

--- ContributorsList.js
+++ ContributorsList.js
@@ -4,4 +4,9 @@ let contributors = [
     name: "Pranjal Agarwal",
     linkedin: "https://www.linkedin.com/in/pranjal-agarwal-11b543226/",
   },
+  {
+    github: "https://github.com/shrey141102",
+    name: "Shreyansh Khaitan",
+    linkedin: "https://www.linkedin.com/in/shreyansh-khaitan/",
+  },
   ];
